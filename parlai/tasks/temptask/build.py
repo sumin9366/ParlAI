448b8ab3e0f2e377a3fab9f4a7778648dad55654
SOONG-E <52056062+SOONG-E@users.noreply.github.com>
2020-09-10 20:56:57 +0900

    Create build.py

---
+++ parlai/tasks/temptask/build.py
@@ -0,0 +1,31 @@
+import parlai.core.build_data import DownloadableFile
+import parlai.core.build_data as build_data
+import os
+
+
+def build(opt):
+    # get path to data directory
+    dpath = os.path.join(opt['datapath'], 'example')
+    # define version if any
+    version = None
+
+    # check if data had been previously built
+    if not build_data.built(dpath, version_string=version):
+        print('[building data: ' + dpath + ']')
+
+        # make a clean directory if needed
+        if build_data.built(dpath):
+            # an older version exists, so remove these outdated files.
+            build_data.remove_dir(dpath)
+        build_data.make_dir(dpath)
+
+        # download the data.
+        fname = 'example.tar.gz'
+        url = 'https://docs.google.com/uc?export=download&id=1M798qKi1PFJiO8KzqJ5N5rQu_kZ61t0Q' # dataset URL
+        build_data.download(url, dpath, fname)
+
+        # uncompress it
+        build_data.untar(dpath, fname)
+
+        # mark the data as built
+        build_data.mark_done(dpath, version_string=version)
