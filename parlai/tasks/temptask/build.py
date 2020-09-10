9e23acc40e84d4d3a71e1e6d75887da418cf062f
SOONG-E <52056062+SOONG-E@users.noreply.github.com>
2020-09-10 21:35:10 +0900

    Update build.py

--- parlai/tasks/temptask/build.py
+++ parlai/tasks/temptask/build.py
@@ -1,4 +1,12 @@
-import parlai.core.build_data import DownloadableFile
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+#
+# Download and build the data if it does not exist.
+
+from parlai.core.build_data import DownloadableFile
 import parlai.core.build_data as build_data
 import os
 
